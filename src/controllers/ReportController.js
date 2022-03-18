17a160657c9183b9c3be333956c209c72d974790
Alex Paris <alexlima@gruposgapetro.com.br>
2022-03-18 05:45:52 -0300

    chore: report test with more complex select

---
+++ src/controllers/ReportController.js
@@ -0,0 +1,35 @@
+const { Op } = require('sequelize');
+
+const Tech = require('../models/Tech');
+const User = require('../models/User');
+
+module.exports = {
+	async show(req, res) {
+		// Find all users with '@paris.dev' mail
+		// Those users need to have an address (or addresses) in 'Rua Madre Sueli'
+		// Those users need to have a tech who start with 'React'
+
+		const users = await User.findAll({
+			attributes: ['name', 'email'],
+			where: {
+				email: {
+					[Op.iLike]: '%@paris.dev'
+				}
+			},
+			include: [
+				{ association: 'addresses', where: { street: 'Rua Madre Sueli' } },
+				{
+					association: 'techs',
+					required: false,
+					where: {
+						name: {
+							[Op.iLike]: 'React%'
+						}
+					}
+				},
+			]
+		});
+
+		return res.json(users);
+	},
+}
